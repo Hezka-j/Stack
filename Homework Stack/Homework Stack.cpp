f673ac09216f12a55d20daa1a17c6e98b973b5a1
Hezka-j <145163687+Hezka-j@users.noreply.github.com>
2024-03-12 23:40:05 +0200

    Add files via upload

---
+++ Homework Stack/Homework Stack.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	stack<char> st;
+	/*string str = "{6-<[5+ ]>+";
+	string str = "{6-<[5+ ]+}";
+	string str = "{6-<[5+ >+}";
+	string str = "{6-<5+ ]>+}";
+	string str = "6-<[5+ ]>+}";*/
+	string str = "}6-<[5+ ]>+{";
+
+	for (int i = 0; i < str.length() + 1; i++)
+	{
+		if(str[i] == '[' || str[i] == '{' || str[i] == '<')
+		{
+			st.push(str[i]);			
+		}
+		if(str[i] == ']' || str[i] == '}' || str[i] == '>')
+		{
+			if (st.top() == '[' && str[i] == ']')
+			{
+				cout << st.top() << "\n";
+				cout << "okay\n";
+				st.pop();
+			}
+			else if (str[i] == '}' && st.top() == '{')
+			{
+				cout << st.top() << "\n";
+				cout << "okay\n";
+				st.pop();
+			}
+			else if (str[i] == '>' && st.top() == '<')
+			{
+				cout << st.top() << "\n";
+				cout << "okay\n";
+				st.pop();
+			}
+			else
+				throw "error";
+		}
+	}
+	if (st.size() > 0)
+		throw " error";
+}
